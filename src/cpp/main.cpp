04ee9abf2b989ab7086ebc005d8e53cf1cfab169
Denis <code4un@yandex.ru>
2018-11-22 09:54:22 +0400

    libs & icon
    added:
     - GLFW static library for win32 x86-32;
     - GLEW static library for win32 x86-32;
     - OpenGL static system-default library ;
     - GLM - OpenGL Math Library ;
     - app icon for win32 ;

---
+++ src/cpp/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * Copyright © 2018 Denis Zyamaev. Email: (code4un@yandex.ru)
+ * License: see "LICENSE" file
+ * Author: Denis Zyamaev (code4un@yandex.ru)
+ * API: C++ 11
+*/
+
+// Include STL
+#include <iostream> // cout, cin, cin.get
+#include <cstdlib> // std
+
+/* MAIN */
+int main( int argC, char** argV )
+{
+
+	// Print "Hello World !"
+	std::cout << "Hello World !" << std::endl;
+
+	// Pause Console-Window
+	std::cin.get( );
+
+	// Return OK
+	return( 0 );
+
+}
